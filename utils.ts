0cbd5fc6c4153c8fbaf6165684820835b73438aa
William Perron <hey@wperron.io>
2021-07-09 09:55:25 -0400

    remove unused imports and rename type

--- utils.ts
+++ utils.ts
@@ -1,8 +1,6 @@
-import { FirehoseClient, PutRecordCommand } from "https://cdn.skypack.dev/@aws-sdk/client-firehose@v3.13.0?dts";
+export type Handler = (r: Request) => Promise<Response>;
 
-export type handler = (r: Request) => Promise<Response>;
-
-export function withLogging(f: handler): handler {
+export function withLogging(f: Handler): Handler {
   return async (req: Request) => {
     const res = await f(req);
     console.log(`${res.status} ${res.statusText} ${new URL(req.url).pathname}`);
